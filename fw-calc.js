762558a7b4d3be674db80727152366c911534c14
Jordan Bugglin <62662632+Jbugglin@users.noreply.github.com>
2025-03-10 22:09:11 -0400

    Update
    I fixed some calculation issues, still have an issue with the percentages going over 100% but shouldn't be too hard to fix. I want to style it so that if the percentage is greater than 100%, it will appear in green and anything else in red.

--- fw-calc.js
+++ fw-calc.js
@@ -4,33 +4,42 @@ function getValues() {
     let heightValue = document.getElementById('stack-height').value;
     let depthValue = document.getElementById('stack-depth').value;
     calcCuFt(lengthValue, heightValue, depthValue);
-    console.log("Function getValues: " +" L: " +lengthValue+ " H: " +heightValue+ " D: " +depthValue);
 }
 
 //Calculates the cubic feet from the user input
 function calcCuFt(lengthValue, heightValue, depthValue) {
     let cubicFt = lengthValue * heightValue * depthValue;
+    let cuftSpan = document.getElementById('cuFtResult');
+    cuftSpan.innerHTML = cubicFt;
     compareFullCord(cubicFt);
     compareFaceCord(cubicFt);
-    console.log("Function calcSqFt: " +cubicFt+ "cu.ft");
+    numberOfCords(cubicFt);
 }
 
 //Need to make a comparative function that compares the user input to the measurement of a full/face cord.
 function compareFullCord(cubicFt) {
     fullCordCuFt = 128;
     let cordPercentage = 0;
-    let largeCord = 0;
+    let fullCordDisp = document.getElementById('fullCordResult');
     if (cubicFt > fullCordCuFt) {
-        largeCord = ((cubicFt - fullCordCuFt) / fullCordCuFt) * 100;
-        console.log(largeCord+ " % over a full cord");
+        cordPercentage = ((cubicFt - fullCordCuFt) / fullCordCuFt) * 100;
+        fullCordDisp.innerHTML = cordPercentage;
     } else {
         cordPercentage = (cubicFt / fullCordCuFt) * 100;
-        console.log(cordPercentage+ " % of a full cord");
+        fullCordDisp.innerHTML = cordPercentage;
     }
 }
 
 //Face cord 1/3 of size as normal cord, 4ft x 8ft x ~16in (l x h x w)
-function compareFaceCord(fullCordCuFt) {
+function compareFaceCord(cubicFt) {
     let faceCordCuFt = 42.66;
-    console.log(faceCordCuFt)
+    let faceCordPercentage = 0;
+    let faceCordDisp = document.getElementById('faceCordResult');
+    if (cubicFt > faceCordCuFt){
+        faceCordPercentage = ((cubicFt - faceCordCuFt) / faceCordCuFt) * 100;
+        faceCordDisp.innerHTML = faceCordPercentage;
+    } else {
+        faceCordPercentage = (cubicFt / faceCordCuFt) * 100;
+        faceCordDisp.innerHTML = faceCordPercentage;
+    }
 }
